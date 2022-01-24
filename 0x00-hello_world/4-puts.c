357555da8a34a31d811fab7226d55910b94531db
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-24 15:20:56 -0800

    Q4Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /**
  * main - the entery point
  *
@@ -8,7 +6,7 @@
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle"
-       );
-  return (0);
-    }
+puts("\"Programming is like building a multilingual puzzle"
+);
+return (0);
+}
