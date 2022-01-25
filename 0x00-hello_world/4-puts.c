bfcc74d484af85328e6b8b28404717f5e8b2a9db
jarrettjewart <92384372+jarrettjewart@users.noreply.github.com>
2022-01-24 22:11:36 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+
 /**
- * main - entry point
+ * main - The entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 - exits with return line 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle"
+);
+return (0);
 }
