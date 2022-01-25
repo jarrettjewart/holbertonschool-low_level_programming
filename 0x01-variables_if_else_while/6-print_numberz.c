664682232e05ed7fbcc9127ea43cf4993ff19351
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-25 15:42:37 -0800

    Q6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main Prints the alpha in lowercase
+ *
+ * Return: returns to 0
+ */
+int main(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
