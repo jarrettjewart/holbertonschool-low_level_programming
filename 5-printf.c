c0bd9f5c6a6a5baf40b44e7c848554c2809664d2
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-24 19:16:11 -0800

    Q5

---
+++ 5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - enters the program
+ *
+ * Return: returns the value 0 and close the program
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n);
+return (0);
+}
