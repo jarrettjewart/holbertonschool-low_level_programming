5ab984182d7c4f47281e2dae0581748168a6c71a
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-24 16:45:24 -0800

    Q4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-
 /**
- * main - The entry point
+ * main - entry point
  *
- * Return: 0 - exits with return line 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle"
-       );
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
