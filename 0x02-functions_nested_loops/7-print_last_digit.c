aad06071d600023670cd6c734be3c220afc8f43e
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 15:47:40 -0800

    Q7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,23 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* print_last_digit - program start
+* @n: int
+*
+* Return: return (0)
+*/
+int print_last_digit(int n)
 {
-int r;
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
+n = (n % 10);
+if (n >= 0)
+{
+_putchar('0' + n);
+return (n);
+}
+else if (n < 0)
+{
+n = (n * -1);
+_putchar('0' + n);
+return (n);
+}
 return (0);
 }
