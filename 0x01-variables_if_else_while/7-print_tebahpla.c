9ddb4827ceac48bb47dcffc6bc471d944deb0eb6
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-25 16:47:02 -0800

    Q7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main Prints the alpha in lowercase
+ *
+ * Return: returns to 0
+ */
+int main(void)
+{
+char x;
+for (x = 'z'; x >= 'a'; --x)
+putchar(x);
+putchar('\n');
+return (0);
+}
