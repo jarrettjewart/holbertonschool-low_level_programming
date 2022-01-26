ad67a9b0989e53374205060ee2acf669c1b32ddc
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-25 19:07:34 -0800

    Q8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#!include <stdio.h>
+/**
+*main - entry point
+*Return: Desc of value
+*/
+int main(void)
+{
+int x;
+char y;
+for (x = 0; x < 10; x++)
+putchar(x % 10 + '0');
+for (y = 'a'; y <= 'f'; y++)
+putchar(y);
+putchar('\n');
+return (0);
+}
