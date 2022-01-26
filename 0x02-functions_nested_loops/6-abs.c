911f446a5a5dab1d64e291024b571bb18f7a57f4
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 14:41:21 -0800

    Q6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _abs - Check the code
+* @n: N is exact number
+* Return: (0)
+*/
+int _abs(int n);
+{
+if (n >= 0)
+{
+return (n);
+}
+else if (n <)
+{
+return (-n);
+}
+return (0);
+}
