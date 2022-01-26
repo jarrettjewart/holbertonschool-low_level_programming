8364527274ec63aa9b574d000f2064aec7aa0983
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 11:33:02 -0800

    Q1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - runs program
+*
+* putchar prints alphabet
+*
+* Return: (0)
+*/
+int main(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+_putchar(x);
+_putchar(\n);
+return (0);
+}
