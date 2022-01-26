54851dbbf41d103fcbd50412b6cf1ad27dabf75f
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 13:24:26 -0800

    Q3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _islower - (inct c)
+* @c: C is the integer
+* Return: is (0)
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+else
+return (0);
+}
+}
