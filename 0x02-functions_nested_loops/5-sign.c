6006dc3f399b0f68164fde2ca30b92e7558ad57b
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 14:22:30 -0800

    Q5v2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,20 +7,20 @@
 int print_sign(int n)
 {
 if (n > 0)
-_putchar(+);
+_putchar('+');
 {
 return (1);
 }
 else if (n == 0)
-_putchar(0);
+_putchar('0');
 {
 return (0);
 }
 else
-_putchar(-);
+_putchar('-');
 {
 return (-1);
 }
-_putchar('\n';)
+_putchar('\n');
 return (0);
 }
