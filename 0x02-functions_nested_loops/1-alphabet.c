cccd052130c40d7d4a6993b3dc81b9a09bdd023b
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 12:48:00 -0800

    Q1revamp#2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
-* main - runs program
+* print_alphabet - runs program
 *
 * putchar prints alphabet
 *
 * Return: (0)
 */
-int main(void)
+void print_alphabet(void)
 {
 char x;
 for (x = 'a'; x <= 'z'; x++)
 _putchar(x);
 _putchar('\n');
-return (0);
 }
