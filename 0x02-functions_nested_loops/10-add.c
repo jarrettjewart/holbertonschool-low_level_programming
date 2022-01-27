81b6e72301bc78aa554c1bc0cd7cbc10041aa2ad
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 19:39:28 -0800

    Q10revamp

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds numbers
+ * @x: first variable
+ * @y: second variable
+ * Return: (0)
+ */
+int add(int x, int y)
+{
+int result;
+{
+result = x + y;
+}
+return (result);
+}
