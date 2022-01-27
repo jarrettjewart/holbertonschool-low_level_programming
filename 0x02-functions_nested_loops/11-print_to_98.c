c1c20c3a388922ad2a4e1b4f4d248cc1d358af39
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 20:12:39 -0800

    11revampv2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 if (n >= 98)
 {
 while (n > 98)
-}
+{
 printf("%i, ", n--);
 }
 printf("%i\n", n);
@@ -19,6 +19,7 @@ else
 while (n < 98)
 {
 printf("%i, ", n++);
+}
 printf("i\n", n);
 }
 }
