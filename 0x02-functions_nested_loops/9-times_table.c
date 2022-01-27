322c7e6059d5a39c50c0099a05802843d049076b
jarrettjewart <jarrett_jewart@yahoo.com>
2022-01-26 18:47:02 -0800

    Q9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* times_table - Multiply by 9
+*
+* Return (0)
+*/
+void times_table(void)
+{
+int length;
+int height;
+int product;
+for (length = 0; length <= 9; length++)
+{
+putchar('0');
+for (height = 1; height <= 9; height++)
+{
+product = length * height;
+putchar(',');
+putchar(' ');
+if (product <= 9)
+{
+putchar(' ');
+}
+else
+{
+putchar((product / 10) + '0');
+}
+putchar((product % 10) + '0');
+}
+putchar('\n');
+}
+}
