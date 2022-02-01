b5eb620a9db68c8ca84f92738f758568a557a46c
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-01 13:41:54 -0800

    Q2 v4

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * mul - multiples a and b
- * *Return: (0)
+ * @a: Inter a
+ * @b: Inter b 
+ * Return: (0)
  */
 int mul(int a, int b)
 {
 product = int a * int b;
 {
-return (0)
+return (0);
 }
 }
