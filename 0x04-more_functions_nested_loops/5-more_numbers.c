202d96107ef1fe833dd58431e11b020280e20af5
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-01 15:12:33 -0800

    Q5v2

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check code
+ * Return: 0
+ */
+void more_numbers(void)
+{
+int x;
+int y;
+for (x = 1; x <= 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y > 9)
+{
+_putchar(y / 10 + '0');
+}
+_putchar(y % 10 + '0');
+}
+_putchar('\n');
+}
+}
