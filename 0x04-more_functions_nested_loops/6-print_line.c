fe8a1905b0429c89b720645dfa04e409bd76cdce
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-01 15:58:43 -0800

    Q6v4

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"/**
+* print_line - function to print the line
+* @n: number of spaces inputted
+* Return: void
+*/void print_line(int n)
+{
+int line;	
+if (n > 0)
+{
+for (line = 0; line < n; line++)
+{
+_putchar(95);
+}
+}
+_putchar('\n');
+}
