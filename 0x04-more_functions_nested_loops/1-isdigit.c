d98b2cd96293d8df742cd964bc48e7d9e944c38f
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-01 08:45:01 -0800

    First set

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - return digit c as 1
+ * @c: C is 1
+ * Return: (0)
+ */
+int _isdigit(int c);
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+#include <unistd.h>
+/* prototypes*/
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
+
+#endif /* MAIN.H */
