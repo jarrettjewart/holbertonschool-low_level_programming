d600935f21a3a9b9cfe885bc905a1ee97b94064a
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-01 14:35:14 -0800

    Q3v3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main"
+/**
+ * print_numbers - void code
+ * Return: 0
+ */
+int print_numbers(void)
+}
+int x;
+for (x = 0; x == 9, x++)
+{
+_putchar(x);
+}
+{
+_putchar('\n');
+}
+{
+return (0);
+}
+}
