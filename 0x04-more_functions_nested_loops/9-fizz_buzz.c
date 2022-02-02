d2fec1119b2827c638ced0556f0921dd3432b7ed
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-01 19:34:55 -0800

    Q9v5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,14 +9,27 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		printf("FizzBuzz ");
-		if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else
+		if (((i % 3 == 0) && ((x % 5) == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf(" Fizz");
+			}
+		else if ((i % 5) == 0)
+		{
+		printf(" Buzz");
+		}
+		else if (x == 1)
+		{
 			printf("%d ", i);
+			}
+			else
+			{
+			printf(" %d, i);
+			}
+			}
 		printf("\n");
-	}
 	return (0);
 }
