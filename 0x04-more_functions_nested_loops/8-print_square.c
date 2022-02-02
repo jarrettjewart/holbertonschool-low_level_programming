34b8c429ee0472637f1a09b9ca0229379109a8a7
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-01 17:37:07 -0800

    spacing

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - Square
+ * @size: siz of square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x;
+	int y:
+
+		if (size > 0)
+		{
+			for (x = 0; x < size; x++)
+			{
+				for (y = 0; y < size; y++)
+				{
+					_putchar(35);
+				}
+				_putchar('\n');
+			}
+		}
+		else
+		{
+			_putchar('\n')
+		}
+}
