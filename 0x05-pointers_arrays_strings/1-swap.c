b3387a147e64ca48a09b02dc7bc946afe89c9904
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-02 11:44:04 -0800

    Q1v4

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,12 +3,14 @@
  * swap_int - swaps intergers
  * @a: 1st int
  * @b: 2nd int
+ * @temp: 3rd int
  * Return: 0
  */
 void swap_int(int *a, int *b)
 {
-	int a, b;
-	a = a + b;
-	b = a - b;
-	a = a - b;
+	int temp;
+
+	temp = *b;
+	*b = *a;
+	*a = temp;
 }
