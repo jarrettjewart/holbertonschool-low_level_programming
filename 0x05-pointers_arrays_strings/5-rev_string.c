ba7c8e4ec3e2268680bcd105cbace8dcab8c5d91
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-02 13:18:37 -0800

    Q5v3

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,16 @@ void rev_string(char *s)
 {
 	int x;
 	int y;
+	char rev;
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
 		continue;
 	}
-	for (y = x - 1; y >= 0; y--)
+	for (y = x - 1; y >= x / 2; y--)
 	{
-		_putchar(s[y]);
+		reverse = s[y]
+			s[y] = s[x - y - 1];
+		s[x - y -1] = reverse;
 	}
 }
