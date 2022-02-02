a5d0ea7eebe71c76f617be0c5188d99fd8a7e4ff
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-01 17:44:07 -0800

    Q8v6

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,22 +6,22 @@
  */
 void print_square(int size)
 {
-	int x;
-	int y:
+int x;
+int y:
 
-		if (size > 0)
-		{
-			for (x = 0; x < size; x++)
-			{
-				for (y = 0; y < size; y++)
-				{
-					_putchar('#');
-				}
-				_putchar('\n');
-			}
-		}
-		else
-		{
-			_putchar('\n')
-		}
+if (size > 0)
+{
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n')
+}
 }
