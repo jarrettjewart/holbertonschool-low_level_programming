a979a73873b4d753c305c59389c8291ce92a985e
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-02 12:56:57 -0800

    Q4v3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints reverse string
+ * @s: first int
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int x;
+	int y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		continue;
+	}
+	for (y = x - 1; y >= 0; y--)
+	{
+		_puchar(s[y]);
+	}
+	_putchar('\n');
+}
