81176b627fc8246fa0a207c653d032d832c43072
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-02 12:15:02 -0800

    Q2v2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _strlen - String Length
+ *  @s: char 1
+ *  Return: x
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; *s[x] != '\0'; x++)
+	{
+		continue;
+	}
+	return (x);
+}
+
