52c944b88829bea99393491ad22d38cba0b84db1
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-03 12:37:55 -0800

    Q9v4

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,16 +7,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int x;
-
-	for (x = 0; src[x] != '\0', x++)
-	{
 		size_t i;
 
-		for (1 = 0; src[i] != '\0'; i++)
+		for (i = 0; src[i] != '\0'; i++)
 		{
 			dest[i] = src[i];
 		}
 		dest[i] = '\0';
 		return (dest);
-	}
+	
+}
