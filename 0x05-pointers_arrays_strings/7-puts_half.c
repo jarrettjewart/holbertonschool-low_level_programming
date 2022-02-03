8209ce234e0ebc60c8eefb3b09a69e5c670f67cd
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-03 09:19:21 -0800

    Q7v2

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - prints half of string 
+ * @str: string
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int length;
+	int s;
+	int half;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+		continue;
+	}
+	if ((length % 2) == 0)
+	{
+		half = (length / 2);
+	}
+	else
+	{
+		half = ((length +1) / 2);
+	}
+	for (s = half; s < length; s++)
+	{
+		_putchar(str[s]);
+	}
+	_putchar('\n');
+}
