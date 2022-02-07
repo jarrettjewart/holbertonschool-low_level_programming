8be7cc6eccefd1dec472c587d0f51503e4f2fb6e
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-07 11:53:44 -0800

    Q1v5

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
-	int des_len;
+	int destlen;
 
-	for (destlen = 0; dest[destlen] !='\0'; destlen++)
+	for (destlen = 0; dest[destlen] != '\0'; destlen++)
 	{}
 	for (i = 0; i < n; i++)
 	{
 		dest[destlen + i] = src[i];
-		if (src[i] = '\0')
+		if (src[i] == '\0')
 			break;
 	}
 	dest[destlen + i] = '\0';
