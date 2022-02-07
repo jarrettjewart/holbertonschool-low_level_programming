f46c76058b25cfb044ac89980f2df2f6608524f2
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-07 11:14:53 -0800

    Q1v1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strncat - combines 2 strings
+ * @dest: dest
+ * @src: src
+ * @n: n
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int des_len;
+
+	for (destlen = 0; dest[destlen] !='\0'; destlen++)
+	{}
+	for (i = 0; i < n; i++)
+	{
+		dest[destlen + i] = src[i];
+		if (src[i] = '\0')
+			break;
+	}
+	dest[destlen + i] = '\0';
+	return (dest);
+}
