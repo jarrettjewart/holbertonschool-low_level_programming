e34d6ffa81c5d169669a263b9c1f3f4df2e54d55
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-08 12:12:05 -0800

    Q3v3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * pointers_arrays - main function
+ * @s: 1 vari
+ * @accept: 2 vari
+ * Return: s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int x,y;
+
+	for (x = 0; s[x] != 10; x++)
+	{
+		for (y = 0; accept[x] != '\n'; y++)
+		{
+			if (accept[y] == s[x])
+			{
+				break;
+			}
+			if (accept[y] == '\0')
+			{
+				return (x);
+			}
+		}
+	}
+	return (0);
+}
