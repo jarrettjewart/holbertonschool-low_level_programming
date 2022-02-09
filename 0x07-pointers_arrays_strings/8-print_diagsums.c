4b49f7ca618e6751f466484537cdbda26b9defd8
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-08 20:11:45 -0800

    Q7v4

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include 'main.h"
+/**
+ * print_diagsums - main function
+ * @a: 1 vari
+ * @size: 2 vari
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	unsigned int x = 0;
+	unsigned int y = 0;
+	int sum;
+
+	for (sum = 0; sum < size; sum++)
+	{
+		x = x + a[(size * sum) + sum];
+		y = y + a[(size * (sum + 1)) - (sum +)];
+	}
+	printf("%d, %d\n", x, y);
+}
