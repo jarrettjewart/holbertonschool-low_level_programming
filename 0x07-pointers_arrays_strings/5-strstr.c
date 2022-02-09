b54da3d80d724066985eaad9334967b867d1b6da
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-08 19:02:50 -0800

    Q5v5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strstr - main function
+ * @haystack: 1 vari
+ * @needle: 2 vari
+ * Return: 0
+ */
+{
+	int a;
+	int b;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+		}
+		if (!needle[b])
+		{
+			return (&haystack[a]);
+		}
+	}
+	return (NULL);
+}
