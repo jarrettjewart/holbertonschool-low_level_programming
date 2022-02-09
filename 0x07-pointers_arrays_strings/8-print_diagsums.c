56723817e58c6e9d6d24355f2ca9300f31541403
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-08 20:17:13 -0800

    Q8v6

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,10 @@
-#include 'main.h"
+#include "main.h"
 /**
- * print_diagsums - main function
- * @a: 1 vari
- * @size: 2 vari
- * Return: 0
- */
+* print_diagsums - main function
+* @a: 1 vari
+* @size: 2 vari
+* Return: 0
+*/
 
 void print_diagsums(int *a, int size)
 {
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 	for (sum = 0; sum < size; sum++)
 	{
 		x = x + a[(size * sum) + sum];
-		y = y + a[(size * (sum + 1)) - (sum +)];
+		y = y + a[(size * (sum + 1)) - (sum + 1)];
 	}
 	printf("%d, %d\n", x, y);
 }
