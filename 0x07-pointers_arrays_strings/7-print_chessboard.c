96049717f0b07c50a7e527f6a9487d917e502992
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-08 19:20:55 -0800

    Q7v2

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - mainfunction
+ * @a: 1 vari
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; a[x][7]; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
