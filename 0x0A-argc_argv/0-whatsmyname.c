8e5fe05fd0914f0f10da0284d8a9ea6b823b5884
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-15 19:20:31 -0800

    Q0v3

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
