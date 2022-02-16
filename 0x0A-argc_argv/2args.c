d86c4b679e2de3ec2547722f4a69882c5c03f465
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-15 19:26:09 -0800

    Q2v5

---
+++ 0x0A-argc_argv/2args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; i < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
