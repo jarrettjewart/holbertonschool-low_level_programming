0392e46963311bea2bb58ee18975c6f1b1a5c920
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-23 09:46:10 -0800

    Q3 full files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if (argc !+ 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	x = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(x, y));
+	return (0);
+}
