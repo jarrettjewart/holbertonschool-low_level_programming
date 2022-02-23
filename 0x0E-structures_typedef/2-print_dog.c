7712df88063a40aee8173241bfd7517d8f9dceff
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-22 17:23:13 -0800

    Q2 fixing errors for check 8

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name == NULL ? "(nil)" : d->name));
-		printf("Age: %f\n", (d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", (d->owner == NULL ? "(nul)" : d->owner));
 	}
 }
