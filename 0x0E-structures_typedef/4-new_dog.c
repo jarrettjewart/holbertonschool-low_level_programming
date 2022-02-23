8d6b13f0e7582d45ade4b231e2de0698b2de67dd
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-22 20:13:46 -0800

    Q4 for loop updates and fixing the lengths

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,15 +10,39 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *cpyname, *cpyowner;
+	int namelen, ownerlen, i, x;
 
+	for (namelen = 0; name[namelen] != '\0'; namelen++);
+	namelen++;
+	for (ownerlen = 0; owner[ownerlen] != '\0'; ownerlen++);
+	ownerlen++;
+	cpyname = malloc(sizeof(char) * namelen);
+	if (cpyname == NULL)
+		reutrn (NULL);
+	cpypwner = malloc(sizeof(char) * ownerlen);
+	if (cpypwner == NULL)
+	{
+		free(cpyname);
+		return (NULL);
+	}
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
-		free(new_dog);
+		free(cpyname);
+		free(cpyowner);
 		return (NULL);
 	}
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
+	for (i = 0; i < namelen; i++)
+	{
+		cpyname[i] = name[i];
+	}
+	for (x = 0; x < ownerlen; x++)
+	{
+		cpyowner[x] = owner[x];
+	}
+	dog_t->name = cpyname;
+	dog_t->age = age;
+	dog_t->owner = cpyowner;
+	return (dog_t);
 }
