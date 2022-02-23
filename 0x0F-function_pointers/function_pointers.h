23cdb6078c32b732be091c05c8ec9d0556e18087
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-23 06:51:40 -0800

    Q0v2

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS-H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c)
+
+
+
+
+#endif /* FUNCTION_POINTERS_H */
