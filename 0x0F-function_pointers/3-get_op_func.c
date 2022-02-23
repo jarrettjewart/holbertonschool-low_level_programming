8809afdf4bf6e98c3d402f54381c3122744fd55d
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-23 10:38:08 -0800

    Q3 fixing gcc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,8 +18,11 @@ int (*get_op_func(char *s))(int,int)
 			     };
 	     int i = 0;
 
-	     while (ops[i].op != NULL && *(ops[i].op) != *s)
+	     while (ops[i].op != NULL)
+	     {
+		     if (*(ops[i]).op == *s)
+			     break;
 		     i++;
-
-	     return (ops[i].f);
+	     }
+	return (ops[i].f);
 }
