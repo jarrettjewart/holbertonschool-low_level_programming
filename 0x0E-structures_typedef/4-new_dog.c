96821f702281050406d3876810827f0caaddbb63
jarrettjewart <jarrett_jewart@yahoo.com>
2022-02-22 17:48:23 -0800

    Q4 fixing != Null to == Null

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	struct dog *new_dog;
 
 	new_dog = malloc(sizeof(struct dog));
-	if (new_dog != NULL)
+	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
 	new_dog->age = age;
