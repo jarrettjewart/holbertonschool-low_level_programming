c89301d81e90f79569e6cc3e490981056f2859e4
jarrettjewart <jarrett_jewart@yahoo.com>
2022-03-07 20:56:30 -0800

    Q2v5

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * _strlen - finds streng length
+ * @str: string
+ * Return: lgth of str
+ */
+
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node - adds node to linked list
+ * @head: ptr to linked list 
+ * @str: str to add
+ * Return: node added
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
