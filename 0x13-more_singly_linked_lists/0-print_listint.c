bc3634380dcfdbafa1721dba25a6edcbf748ae27
jarrettjewart <jarrett_jewart@yahoo.com>
2022-03-09 02:08:13 -0800

    Q0 listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints function of listint_t
+ * @h: list
+ * Return: list
+ */
+
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++
+	}
+	return (count);
+}
