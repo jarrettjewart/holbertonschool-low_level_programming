8571f4d2d023b24148827294746bbdf9f670b070
jarrettjewart <jarrett_jewart@yahoo.com>
2022-03-09 02:43:11 -0800

    Q5 freeing memory using a dbl ptr

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees memory in the list
+ * @head: double ptr in the list
+ */
+
+void free_listint2(listint_t **)
+{
+	listint_t *update;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		update = (*head)->update;
+		free(*head);
+		*head = udpdate;
+	}
+}
