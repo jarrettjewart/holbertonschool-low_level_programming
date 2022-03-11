0da88e8e7d74f745455e787da12313c545aa79d6
jarrettjewart <jarrett_jewart@yahoo.com>
2022-03-10 18:45:42 -0800

    Q0 print a list and all of its functions

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,24 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elemetns of list_t
- *
- *
- * Return: printf
+ * print_list - prints the list 
+ * @h:ptr at start of the list
+ * Return: nodes of the list
  */
 
-
 size_t print_list(const list_t *h)
+{
+	const list_t *tmp;
+	unsigned int i;
+
+	tmp = h;
+	for (i = 0; tmp; i++)
+	{
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+	}
+	return (i);
+}
 
 	
 	
