e5b09207e0ab47e870fa7a35bd62263028b1b3bf
jarrettjewart <jarrett_jewart@yahoo.com>
2022-03-15 08:20:08 -0700

    Q0 betty and gcc error fixes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,29 +11,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t lenr, lenw;
-	char *buffer
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
-		return (0)
+		return (0);
 	}
 	lenr = read(fd, buffer, letters);
-	close (fd);
+	close(fd);
 	if (lenr == -1)
 	{
-		free (buffer);
+		free(buffer);
 		return (0);
 	}
 	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free (buffer);
+	free(buffer);
 	if (lenr != lenw)
 		return (0);
-	renturn (lenw);
+	return (lenw);
 }
