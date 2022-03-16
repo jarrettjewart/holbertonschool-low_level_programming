1f4fb6a40b97858f0e6c9793087096516a39c71d
jarrettjewart <jarrett_jewart@yahoo.com>
2022-03-15 19:16:37 -0700

    Q1 betty and gcc error fixed

---
+++ 1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the length of string
+ * @str: ptr to string
+ * Return: length of string
+ */
+
+sze_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * create_file - creates file
+ * @filename: name of file
+ * @text_content: NULL term string
+ * Return: 1 and -1 if fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd
+		ssize_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (len == -1)
+		return (-1);
+	return (1);
+}
